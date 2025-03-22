32696a505ef52c652089eb591ae708bcebd05b8b
DAYANA <est.daibarrab0201@0201.uml.edu-ni>
2025-03-22 00:50:18 -0600

    Añadir archivo principal para probar las clases

---
+++ index.js
@@ -0,0 +1,8 @@
+const Libro = require('./Libro');
+const Ebook = require('./Libro');
+
+// Crea instancias de ambas clases y muestra sus detalles en la consola
+let libro1 = new Libro();
+libro1.mostrarDetalles();
+let libro2 = new Ebook();
+libro2 = mostrarDetalles()
