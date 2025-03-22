43e60a69ee4253b73f258f93db99daa029cd2fa5
DAYANA <est.daibarrab0201@0201.uml.edu-ni>
2025-03-22 00:41:46 -0600

    Añadir subclase Ebook

--- Libro.js
+++ Libro.js
@@ -8,3 +8,15 @@ class Libro {
   
   // Exporta la clase
   module.exports = Libro; 
+
+
+  class Ebook extends Libro {
+    constructor(titulo, autor, añoPublicacion, precio, formato) {
+    }
+    
+    mostrarDetalles() {
+  
+    }
+  }
+  
+  // Añade la exportación de la clase Ebook  
